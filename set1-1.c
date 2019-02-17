cea71f880f75e3e80a12056603086ed01d706bcd
subash789 <46813541+subash789@users.noreply.github.com>
2019-02-17 21:38:22 +0530

    Create set1-1.c

---
+++ set1-1.c
@@ -0,0 +1,21 @@
+#include<stdio.h> 
+ #include<conio.h> 
+ int main() 
+ { 
+ int n; 
+ printf("enter the number:"); 
+ scanf("%d",&n); 
+ if(n>0) 
+ { 
+ printf("%d is positive",n); 
+ } 
+else if(n<0) 
+ { 
+ printf("%d is negative",n); 
+ } 
+ else 
+ { 
+ printf("Is zero number"); 
+} 
+ return 0; 
+ } 
