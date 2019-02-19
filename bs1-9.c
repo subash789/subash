2eb5cb93eb69c5b682546d9a8b37f967a11eb4b0
subash789 <46813541+subash789@users.noreply.github.com>
2019-02-20 00:05:41 +0530

    Create bs1-9.c

---
+++ bs1-9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>  
+    int main()
+    {
+      int n,i,k,sum=0,a[100];
+	    scanf("%d",&n);
+      scanf("%d",&k);
+      for(i=0;i<n;i++)
+      {
+        scanf("%d",&a[i]);
+      }
+	    for(i=0;i<k;i++)
+      {
+        sum=sum+a[i];
+      }    
+      printf("%d",sum);
+      return 0;
+    }
